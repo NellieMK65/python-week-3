1b0a447b784da5462c12675433cd14eac80b3275
Nelson Muriithi <nelsonmuriithi65@gmail.com>
2025-05-21 10:26:55 +0300

    many to many

--- relationships-continued.py
+++ relationships-continued.py
@@ -85,3 +85,102 @@ class Parent(Person):
 parent = Parent("Jane", 39)
 parent.reading()
 parent.pay_fees()
+
+
+# Many to Many (Student and Courses)
+
+class Student:
+    def __init__(self, name):
+        self.name = name
+        # this will be used to store a list of courses a student can do
+        self.courses = []
+
+    def enroll(self, course):
+        # only course if it does not exist
+        if course not in self.courses:
+            self.courses.append(course)
+            # add a reverse relationship
+            course.students.append(self)
+        # else:
+        #     raise ValueError(f"Student {self.name} has is already enrolled to course {course.title}")
+
+
+
+    def __repr__(self):
+        return f"Student {self.name} has courses {[c.title for c in self.courses]}"
+
+
+class Course:
+    def __init__(self, title):
+        self.title = title
+        # this can be used to store all students that belong to a course
+        self.students = []
+
+    def __repr__(self):
+        return f"Course {self.title} has students: {[s.name for s in self.students]}"
+
+sd = Course("Software Engineering")
+devops = Course("Devops")
+
+eugene = Student("Eugene")
+eugene.enroll(sd)
+eugene.enroll(sd)
+eugene.enroll(devops)
+
+trevor = Student("Trevor")
+trevor.enroll(devops)
+
+print(eugene)
+print(sd)
+print(devops)
+
+
+class Artist:
+    all_artists = []
+
+    def __init__(self, name):
+        self.name = name
+        self.albums = []
+        self.all_artists.append(self)
+
+    def add_album(self, album):
+        if album not in self.albums:
+            self.albums.append(album)
+            album.artists.append(self)
+
+
+    def __repr__(self):
+        return f"Artist {self.name} has albums {[a.title for a in self.albums]}"
+
+class Album:
+    def __init__(self, title):
+        self.title = title
+        self.artists = []
+
+    def get_all_artists(self):
+        print(self.artists)
+
+    def __repr__(self):
+        return f"Album {self.title} by artists: {[a.name for a in self.artists]}"
+
+artist = Artist("Kulindeezy")
+artist2 = Artist("Beyonce")
+artist3 = Artist("Ruth")
+artist4 = Artist("Bett")
+print("All artists", [a.name for a in Artist.all_artists])
+
+album1 = Album("Cowboy carter")
+# print(album1.get_all_artists())
+album2 = Album("What a time to be alive")
+
+album3 = Album("The book of jones")
+
+artist.add_album(album1)
+artist2.add_album(album1)
+artist2.add_album(album2)
+
+print(album1)
+print(album2)
+print(artist2)
+print(artist)
+
